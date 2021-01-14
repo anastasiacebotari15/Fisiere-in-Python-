3e85fe6329c693b22842943c3a27c692efd591db
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2021-01-14 11:59:25 +0300

    Delete file3.py

--- file3.py
+++
@@ -1,7 +0,0 @@
-with open ('globulet.txt', 'r') as f:
-    a=f.readline()
-    r=int(a)+3
-    b=(int(a)+ int(b))-2
-    s=int(a)+int(b)+int(r)
-with open ('bradut,txt', 'w') as f:
-    f.write(str(s))
